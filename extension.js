e8c7dd2106443d112f7903b73880e5f2a8f201da
Johannes Millan <johannes.millan@gmail.com>
2017-04-27 12:54:47 +0200

    make binding items to an action work

--- extension.js
+++ extension.js
@@ -47,16 +47,15 @@ const WindowSessionIndicator = new Lang.Class({
       y_align: Clutter.ActorAlign.CENTER,
       text: 'XXXXXXxsxsxs'
     });
-    //button.connect('button-press-event', _showHello);
     let topBox = new St.BoxLayout();
     topBox.add_actor(button);
-    topBox.add_actor(this.statusLabel);
+    //topBox.add_actor(this.statusLabel);
     this.actor.add_actor(topBox);
 
     this._sessionItems = [];
     this._sessionSection = new PopupMenu.PopupMenuSection();
     this.menu.addMenuItem(this._sessionSection);
-    global.log('super,TEST--------------------------------------------------------------------------');
+    global.log('super, SUPER--------------------------------------------------------------------------');
     this._createMenuItems();
   },
 
@@ -64,96 +63,34 @@ const WindowSessionIndicator = new Lang.Class({
     const HOME_PATH = GLib.get_home_dir();
     const LWSM_SESSION_PATH = HOME_PATH + '/.lwsm/sessionData';
     this.lwsmSessionDir = Gio.file_new_for_path(LWSM_SESSION_PATH);
-    this.current_dir = this.lwsmSessionDir;
-    const enumeratedChildren = this.current_dir.enumerate_children('*', 0, null, null);
-    this.processDirectory(enumeratedChildren);
-    global.log('super', this.current_dir);
-    global.log('super', enumeratedChildren);
+    const enumeratedChildren = this.lwsmSessionDir.enumerate_children('*', 0, null, null);
 
-    const list = [
-      'YEEE',
-      'sdfsd',
-      'dfgdfgdfg',
-    ];
-    for (let i = 0; i < list.length; i++) {
-      let listItem = list[i];
-      this._sessionItems[i] = new PopupMenu.PopupMenuItem(HOME_PATH + listItem);
-      this._sessionSection.addMenuItem(this._sessionItems[i]);
-      //this._sessionItems[i].connect('activate', Lang.bind(this, function (actor) {
-      //  this._activate(actor);
-      //}));
-    }
-  },
-
-  processDirectory: function (children) {
+    let fileInfo;
     let files = [];
-    let dirs = [];
-    let fileInfo = null;
-    while ((fileInfo = children.next_file(null, null)) !== null) {
+    while ((fileInfo = enumeratedChildren.next_file(null, null)) !== null) {
       if (!fileInfo.get_is_hidden() && !isDirectory(fileInfo)) {
         files.push(fileInfo);
       }
     }
-    children.close(null, null);
-    global.log('super', files);
+    enumeratedChildren.close(null, null);
 
-    //files.sort(fileComparator);
-    //files.forEach(Lang.bind(this, function (fi) {
-    //  this.filesList.addMenuItem(this.createItem(fi));
-    //}));
+    files.forEach(Lang.bind(this, function (file) {
+      this._sessionSection.addMenuItem(this.createItem(file));
+    }));
   },
 
-  _refresh: function () {
-    this._loadData(this._refreshUI);
-    this._removeTimeout();
-    this._timeout = Mainloop.timeout_add_seconds(10, Lang.bind(this, this._refresh));
-    return true;
+  createItem: function (fileInfo) {
+    const fileName = fileInfo.get_display_name().replace('.json', '');
+    const item = new PopupMenu.PopupMenuItem(fileName);
+    item.connect('activate', Lang.bind(this, function () {
+      this._activateSession(fileName);
+    }));
+    return item;
   },
 
-  _loadData: function () {
-    let params = {
-      amount: '1000',
-      sourceCurrency: 'CHF',
-      targetCurrency: 'EUR'
-    };
-    _httpSession = new Soup.Session();
-    let message = Soup.form_request_new_from_hash('GET', TW_URL, params);
-    message.request_headers.append('X-Authorization-key', TW_AUTH_KEY);
-    _httpSession.queue_message(message, Lang.bind(this, function (_httpSession, message) {
-        if (message.status_code !== 200)
-          return;
-        let json = JSON.parse(message.response_body.data);
-        this._refreshUI(json);
-      }
-      )
-    );
+  _activateSession: function (sessionName) {
+    global.log('super', sessionName);
   },
-
-  _refreshUI: function (data) {
-    let txt = data.transferwisePayOut.toString();
-    txt = txt.substring(0, 6) + ' CHF';
-    global.log(txt);
-    this.buttonText.set_text(txt);
-  },
-
-  _removeTimeout: function () {
-    if (this._timeout) {
-      Mainloop.source_remove(this._timeout);
-      this._timeout = null;
-    }
-  },
-
-  stop: function () {
-    if (_httpSession !== undefined)
-      _httpSession.abort();
-    _httpSession = undefined;
-
-    if (this._timeout)
-      Mainloop.source_remove(this._timeout);
-    this._timeout = undefined;
-
-    this.menu.removeAll();
-  }
 });
 
 let wsMenu;
